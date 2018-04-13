bbcf21b9d8fbe0c9bd9c6e2c4dd444de81bb8061
Jack <8jdunne@stpatrick.org>
2018-04-13 14:38:35 -0500

    second upload

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const redis = require('redis');
+/* GET users listing. */
+/* prefaced by /user/routename*/
+let client = redis.createClient();
+
+client.on('connect',function(){
+  console.log("connected to Redis");
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,5 +18,26 @@ router.get('/', function(req, res) {
 router.get('/spittoon', function(req,res,next){
   res.render('spittoon');
 });
+
+router.post('/spittoon/reload', function(req,res,next){
+  let id = req.body.id;
+  client.hgetall(id,function(err,obj){
+    if(!obj){
+      res.render('index',{
+        error: 'loogie does not exist',
+        title: 'NO!'
+      });
+    }
+    else{
+      obj.id = id;
+      res.render('spittoon',{
+        loogie:obj
+      });
+    }
+  })
+});
+
+
+
 
 module.exports = router;
